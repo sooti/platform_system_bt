20a769f10704d937b0a495ecfe618ac2cb0dee15
Satish Kodishala <skodisha@codeaurora.org>
2017-04-20 11:03:43 +0530

    Send MT call active indicator prior to SCO connection request
    Some remotes are very strict in receiving the call active
    indicator and SCO connection request order for MT call.
    If CIEV1,1 and SCO connection request are sent back to back
    to SOC, it may send SCO connection request first then CIEV1,1
    which may lead to remotes not rendering SCO audio.
    
    CRs-Fixed: 2036765
    Change-Id: I8523fcf90bbc17c0c9f3823a25ae2fde7fefd5ff

--- device/include/interop.h
+++ device/include/interop.h
@@ -153,6 +153,13 @@ typedef enum {
   // values.
   INTEROP_UPDATE_HID_SSR_MAX_LAT,
 
+  // Some remotes are very strict in receiving the call active
+  // indicator and SCO connection request order for MT call.
+  // If CIEV1,1 and SCO connection request are sent back to back
+  // to SOC, it may send SCO connection request first then CIEV1,1
+  // which may lead to remotes not rendering SCO audio.
+  INTEROP_DELAY_SCO_FOR_MT_CALL,
+
   END_OF_INTEROP_LIST
 
 } interop_feature_t;
